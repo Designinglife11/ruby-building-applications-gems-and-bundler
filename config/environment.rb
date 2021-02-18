42aabe22205158503d0b7bc8f98e025afdd1e25a
Andrew <Designinglife11@gmail.com>
2021-02-18 06:50:15 -0500

    commit tree f0a98bb880976d506192bbcf63da5118c592c4e8

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,14 @@
 # Set up Bundler here following the instructions in the README
+
+# source "https://rubygems.org"
+# gem "rspec"
+# gem "sinatra"
+
+# group :development do
+#   gem "pry"
+# end
+
+# require 'bundler/setup'
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
